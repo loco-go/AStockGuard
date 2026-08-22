460f66d579e9d998944942e021bf01374a407364
locogo <52945069+loco-go@users.noreply.github.com>
2026-08-22 17:38:43 +0800

    V3.4 add unit test dependency for trading rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    testImplementation("junit:junit:4.13.2")
 }
 
 kapt { arguments { arg("room.schemaLocation", "$projectDir/schemas") } }
